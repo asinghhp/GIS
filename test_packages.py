60b603cc1b196f4d71e6749f5bd8371484ae9fe9
asinghhp <a.singh@hhpnz.nz>
2024-11-26 23:37:12 +1300

    starting project

---
+++ test_packages.py
@@ -0,0 +1,15 @@
+import os
+import geopandas as gpd
+import rasterio
+import numpy as np
+from osgeo import gdal
+from scipy.ndimage import gaussian_filter
+import pandas as pd
+import sklearn
+import torch
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+import folium
+
+print("All packages imported successfully!")
